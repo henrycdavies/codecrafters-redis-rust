b2ed2d3a0b54b27bc7a436fa4b4ffd15881422a3
Henry Davies <henrycdavies@icloud.com>
2024-04-28 19:45:43 +0100

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,47 @@
 pub mod array;
 pub mod command;
 pub mod resp;
+pub mod pool;
 pub mod util;
 
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::{io::{Error, Read, Result, Write}, net::{TcpListener, TcpStream}, thread::sleep, time::Duration};
 
 use array::RESPArray;
+use pool::ThreadPool;
 use resp::RESPDataType;
 
 const CRLF: &str = "\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+        if let Ok(_stream) = stream {
+            pool.execute(|| {
                 handle_stream(_stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            });
         }
     }
 }
 
-fn handle_stream(mut stream: TcpStream) {
+fn handle_stream(mut stream: TcpStream) -> Result<()> {
     let mut buf = [0; 512];
-    
-    let mut conn_open = true;
-
-    while conn_open {
-        match stream.read(&mut buf) {
-            Ok(_) => {
-                let first_byte = &buf[0..1][0];
-                let parts = std::str::from_utf8(&buf).unwrap().split(CRLF).collect::<Vec<&str>>();
-                for part in &parts {
-                    println!("{}", part);
-                }
-                let response = match first_byte {
-                    b'*' => RESPArray::get_response(&parts),
-                    _ => "UNIMPLEMENTED"
-                };
-                if response == "UNIMPLEMENTED" {
-                    println!("UNIMPLEMENTED");
-                    return;
-                }
-                stream.write_all(response.as_bytes());
-
-            },
-            Err(_) => {
-                conn_open = false;
-            }
+    while let Ok(_) = stream.read(&mut buf) {
+        let first_byte = &buf[0..1][0];
+        let parts = std::str::from_utf8(&buf).unwrap().split(CRLF).collect::<Vec<&str>>();
+        for part in &parts {
+            println!("{}", part);
         }
-    
+        let response = match first_byte {
+            b'*' => RESPArray::get_response(&parts),
+            _ => "UNIMPLEMENTED"
+        };
+        if response == "UNIMPLEMENTED" {
+            return Err(Error::new(std::io::ErrorKind::InvalidInput, "This function is not yet implemented"));
+        }
+        stream.write_all(response.as_bytes());
     }
+    Ok(())
 }
 
 
